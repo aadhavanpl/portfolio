1c5a75cc01b1a90f4b0548d854edbc013097fba1
aadhavanpl <aadhavanlenin@gmail.com>
2022-05-27 11:21:29 +0300

    few basic changes to index.css

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import resume from "../../assets/resume.pdf"
 import "./header.css"
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header_container">
+        <h5>Hello I'm</h5>
+        <h1>Aadhi</h1>
+        <a href='#contact' className='btn'>Contact</a>
+        <a href={resume} download className='btn'>Resume</a>
+      </div>
+    </header>
   )
 }
 
