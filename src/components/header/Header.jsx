a4f7cb26149fd005b02f01fce677aaf8b0cd0832
aadhavanpl <aadhavanlenin@gmail.com>
2022-05-28 10:33:55 +0300

    modified header section

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import resume from "../../assets/resume.pdf"
 import "./header.css"
+// import me from "../../assets/me.png"
+import { AiFillGithub } from "react-icons/ai"
+import { AiFillLinkedin } from "react-icons/ai"
+import { AiFillInstagram } from "react-icons/ai"
 
 const Header = () => {
   return (
@@ -8,8 +12,20 @@ const Header = () => {
       <div className="container header_container">
         <h5>Hello I'm</h5>
         <h1>Aadhi</h1>
-        <a href='#contact' className='btn'>Contact</a>
-        <a href={resume} download className='btn'>Resume</a>
+
+        <div className='cta'>
+          <a href='#contact' className='btn'>Contact</a>
+          <a href={resume} download className='btn'>Resume</a>
+        </div>
+
+        <div className='me'>
+          {/* <img src={me}/> */}
+        </div>
+        <div className='socials'>
+          <a href='https://github.com/aadhavanpl' target="_blank"><AiFillGithub /></a>
+          <a href='https://www.linkedin.com/in/aadhavan-lenin/' target="_blank"><AiFillLinkedin /></a>
+          <a href='https://www.instagram.com/aadhavan_lenin/' target="_blank"><AiFillInstagram /></a>
+        </div>
       </div>
     </header>
   )
